fc7f23335a9cea1f9f08a78c4fe60146c7e89179
Tanmay98 <tanmaybaweja1999@gmail.com>
2021-05-07 13:37:22 +0530

    first commit

---
+++ Machinecomers.js
@@ -0,0 +1,24 @@
+//By sushant
+
+/* Entities are product, categories, order,location, user and reviews
+
+Get: /products --Used to get all information regarding products such product id, name, price,description, quantity
+Post: /products --Used to add new product to database
+Put: /products/id --Used to make changes to any attribute of product to database
+Delete: /products/id --Used to delate a product from database
+
+Get: /categories --Used to get all information regarding all categories available for products
+
+Get: /order --Used to get order details such as order number, product ordered, availability,date it was ordered 
+Post: /order --Used to send order request that this order has been placed
+
+Get: /user --Used to get user information
+Post: /user --Used add user to database
+Put: /user/id --Used to make change to any user in database
+Delete: /user/id --Used to delate a user from database
+
+Get: /reviews --Used to get all reviews regarding products
+Post: /reviews --Used to post a review for a product
+Put: /reviews/id --Used to make change to a review
+Delete: /reviews/id --Used to delate a review.
+*/
